29c69cad98b3f1f6ac3b73e6bda2ec597d48dd3f
Bret Fisher <bret@bretfisher.com>
2021-07-06 15:34:54 -0400

    swap worker image

--- swarm-app-1/answer/answer.sh
+++ swarm-app-1/answer/answer.sh
@@ -7,7 +7,7 @@ docker service create --name redis --network frontend redis:3.2
 
 docker service create --name db --network backend -e POSTGRES_HOST_AUTH_METHOD=trust --mount type=volume,source=db-data,target=/var/lib/postgresql/data postgres:9.4
 
-docker service create --name worker --network frontend --network backend bretfisher/examplevotingapp_worker:java
+docker service create --name worker --network frontend --network backend bretfisher/examplevotingapp_worker
 
 docker service create --name result --network backend -p 5001:80 bretfisher/examplevotingapp_result
 
